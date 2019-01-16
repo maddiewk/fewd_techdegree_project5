10bb8a611699dcfc6f7be1427877f933e1016bc6
Madeline Watson <mcwatson91@gmail.com>
2019-01-16 18:06:13 -0500

    search function

--- js/app.js
+++ js/app.js
@@ -4,3 +4,21 @@ lightbox.option({
   'showImageNumberLabel': false,
   'wrapAround': true
 })
+
+
+// ============ Search Box Function ================ //
+
+$('#search').on('keyup', function() {
+  const $userInput = $('#search').val().toLowerCase();
+  const $anchor = $('.gall').length;
+
+  for (let i = 0; i < $anchor; i++) {
+    let $currentLink = $('.gall').eq(i);
+    let $caption = $currentLink.attr('data-title');
+    if ($caption.indexOf($userInput) > -1) {
+       $currentLink.css('display', '');
+     } else {
+       $currentLink.css('display', 'none');
+     }
+   }
+});
